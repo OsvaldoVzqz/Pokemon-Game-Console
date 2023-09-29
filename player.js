894c8a730a5d5b5a57c33ca625ff92eaf36772d3
Victor <veom104@gmail.com>
2023-09-28 20:46:31 -0700

    se termino el documento player- se espera revicion

--- player.js
+++ player.js
@@ -1,13 +1,31 @@
 class Player {
   constructor(name, species, pokeName, level) {
     // asignar name a un atributo con el mismo nombre
+    this.name = name;
+    this.species = species;
+    this.pokeName = pokeName;
+    this.level = level;
     // crear un Pokemon con el resto de parametros y asignarlo al atributo pokemon
+    this.pokemon = new Pokemon(this);
+    for (const Propiedad in this.pokemon) {
+      if (Object.hasOwnProperty.call(object, Propiedad)) {
+        const element = object[Propiedad];
+        
+      }
+    }
   }
 
   selectMove() {
     // mostrar al usuario los movimientos dosponibles
+    this.pokemon.move('Movimiento 1');
+    this.pokemon.move('Movimiento 2');
     // Volver a pedir si ingresa un movimiento invalido
+    if (options.includes(pokemon.selectMove)) {//necesitamos tener mas informacion
+      break;
+    };
+    alert("Invalid option");
     // Asigna el movimiento con 'setCurrentMove'
+    setCurrenMove()
     // retornar 'true' en caso el usuario apreta Cancel
   }
 }
