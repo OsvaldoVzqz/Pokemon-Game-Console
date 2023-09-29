ca959c28fe670859b0c9e9415234af38ab3e7082
OSVALDO <Osvavazdelatorre@gmail.com>
2023-09-29 11:33:11 -0600

    Metodo batle

--- battle.js
+++ battle.js
@@ -1,45 +1,106 @@
 class Battle {
   constructor(player1, player2) {
-    // asigna los jugadores a sus respectivos atributos
+    this.player1 = player1;
+    this.player2 = player2;
   }
 
   start() {
-    // Anunciar "The battle is about to start!"
-    // preparar la batalla con prepareBattle()
-    // Usar un bucle para todos los turnos
-    // Ambos jugadores seleccionan un movimiento
-    // Si al seleccionar un movimiento, retorna 'true' terminar la batalla y anunciar "[nombre] run away!"
-    // Calcular quien atacara primero
-    // El primero ataca al segundo
-    // Si el segundo aun no se desmaya...
-    // El segundo ataca al primero
-    // El bucle continua hasta que alguno se desmaye
-    // Al terminar el bucle, identificar al ganador y al perdedor
-    // Anunciar "[perdedor] FAINTED!"
-    // Anunciar "[ganador] WINS!"
-    // Se procesa la victoria
+    console.log("The battle is about to start!");
+    this.prepareBattle();
+
+    while (true) {
+      const move1 = this.player1.selectMove();
+      const move2 = this.player2.selectMove();
+
+      if (move1 === null || move2 === null) {
+        console.log(`${move1 === null ? this.player1.name : this.player2.name} run away!`);
+        break;
+      }
+
+      const firstPokemon = this.getFirstPokemon();
+      const secondPokemon = firstPokemon === this.player1.pokemon ? this.player2.pokemon : this.player1.pokemon;
+
+      firstPokemon.attack(secondPokemon);
+
+      if (secondPokemon.isFainted()) {
+        console.log(`${secondPokemon.name} FAINTED!`);
+        console.log(`${firstPokemon.name} WINS!`);
+        firstPokemon.processVictory(secondPokemon);
+        break;
+      }
+
+      secondPokemon.attack(firstPokemon);
+
+      if (firstPokemon.isFainted()) {
+        console.log(`${firstPokemon.name} FAINTED!`);
+        console.log(`${secondPokemon.name} WINS!`);
+        secondPokemon.processVictory(firstPokemon);
+        break;
+      }
+    }
   }
 
   prepareBattle() {
-    // llamar a prepareForBattle de los pokemones de ambos jugadores
-    // anunciar "[judgador]sent out [POKEMON]!" para ambos jugadores
+    this.player1.pokemon.prepareForBattle();
+    this.player2.pokemon.prepareForBattle();
+    console.log(`${this.player1.name} sent out ${this.player1.pokemon.name}!`);
+    console.log(`${this.player2.name} sent out ${this.player2.pokemon.name}!`);
   }
 
   getFirstPokemon() {
-    // verificar si un pokemon empieza por tener movimiento con mayor prioridad con firstByPriority
-    // verificar si un pokemon empieza por tener  mayor velocidad con firstBySpeed
-    // si no, elegir uno de manera aleatorio
+    const firstByPriorityResult = this.firstByPriority();
+    if (firstByPriorityResult !== null) {
+      return firstByPriorityResult;
+    }
+
+    const firstBySpeedResult = this.firstBySpeed();
+    if (firstBySpeedResult !== null) {
+      return firstBySpeedResult;
+    }
+
+    // Si ninguno tiene prioridad o velocidad distintiva, elige aleatoriamente.
+    const randomIndex = Math.random() < 0.5 ? 0 : 1;
+    return randomIndex === 0 ? this.player1.pokemon : this.player2.pokemon;
   }
 
   firstByPriority() {
-    // retornar el pokemon con movimiento de mayor prioridad. Si igualan, retornar null
+    const move1 = this.player1.pokemon.currentMove;
+    const move2 = this.player2.pokemon.currentMove;
+
+    if (move1.priority > move2.priority) {
+      return this.player1.pokemon;
+    } else if (move2.priority > move1.priority) {
+      return this.player2.pokemon;
+    } else {
+      return null;
+    }
   }
 
   firstBySpeed() {
-    // retornar el pokemon de mayor velocidad. Si igualan, retornar null
+    if (this.player1.pokemon.stats.speed > this.player2.pokemon.stats.speed) {
+      return this.player1.pokemon;
+    } else if (this.player2.pokemon.stats.speed > this.player1.pokemon.stats.speed) {
+      return this.player2.pokemon;
+    } else {
+      return null;
+    }
   }
 
   printBattleStatus() {
-    // usar conole.table para mostrar el status de la batalla (player, pokemon, level, currentHp)
+    console.table([
+      {
+        Player: this.player1.name,
+        Pokemon: this.player1.pokemon.name,
+        Level: this.player1.pokemon.level,
+        CurrentHP: this.player1.pokemon.currentHp,
+      },
+      {
+        Player: this.player2.name,
+        Pokemon: this.player2.pokemon.name,
+        Level: this.player2.pokemon.level,
+        CurrentHP: this.player2.pokemon.currentHp,
+      },
+    ]);
   }
 }
+
