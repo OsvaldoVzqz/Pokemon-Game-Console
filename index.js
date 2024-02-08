ffc3dc344fa9f67c97d5a67926071d8e835640f1
OSVALDO <Osvavazdelatorre@gmail.com>
2024-02-08 17:56:58 -0600

    Pokemon Game

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Versión síncrona:
-// Crear un nuevo Game y llamar al método start
-
-// Versión asíncrona:
-// Iniciar un contador de 10 segundos antes de empezar el juego
-// Inciar un intervalo para mostras los segundos restantes en la consola
-// Recuerda 'cancelar' el intervalo cuando llegue a 0 segundos
 
 let contador = 10;
 
